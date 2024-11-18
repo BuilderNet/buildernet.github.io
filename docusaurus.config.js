b9c2f88a89b37337cabfc9f73309318644444c7e
Chris Hager <chris@linuxuser.at>
2024-11-18 10:03:31 +0100

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) Flashbots Ltd. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+require('dotenv').config();
+const { themes } = require('prism-react-renderer');
+
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
+
+/** @returns {Promise<import('@docusaurus/types').Config>} */
+module.exports = async function createConfigAsync() {
+  return {
+    title: 'BuilderNet Blog',
+    tagline: 'Illuminate Dmocrtz Dstrib Prtct',
+    baseUrl: process.env.BASE_URL,
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'BuilderNet',
+    projectName: 'blog',
+    url: process.env.TARGET_URL,
+    markdown: {
+      mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
+    stylesheets: [
+      {
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+        type: 'text/css',
+        integrity:
+          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        crossorigin: 'anonymous',
+      },
+    ],
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        colorMode: {
+          defaultMode: 'light',
+        },
+        prism: {
+          theme: lightTheme,
+          darkTheme,
+          additionalLanguages: ['solidity', 'typescript', 'bash', 'json', 'javascript', 'yaml', 'toml'], // https://docusaurus.io/docs/markdown-features/code-blocks#supported-languages
+        },
+        navbar: {
+          title: 'BuilderNet Blog',
+          logo: {
+            alt: 'Flashbots Logo',
+            src: 'img/logo.png',
+            target: '/blog/',
+          },
+          items: [
+            { to: 'https://buildernet.github.io/docs', label: 'Docs', position: 'left' }, // or position: 'right'
+            {
+              href: 'https://github.com/orgs/buildernet/repositories',
+              label: 'GitHub',
+              position: 'right',
+            },
+          ],
+        },
+        image: 'assets/social-preview-3.jpg',
+      }),
+    presets: [
+      [
+        '@docusaurus/preset-classic',
+        {
+          blog: {
+            path: 'content',
+            // Simple use-case: string editUrl
+            // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+            // Advanced use-case: functional editUrl
+            editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
+              `https://github.com/buildernet/blog/edit/main/website/${blogDirPath}/${blogPath}`,
+            editLocalizedFiles: false,
+            blogTitle: 'BuilderNet Blog',
+            blogDescription: 'Blog',
+            blogSidebarCount: 5,
+            blogSidebarTitle: 'All our posts',
+            routeBasePath: '/',
+            include: ['**/*.{md,mdx}'],
+            exclude: [
+              '**/_*.{js,jsx,ts,tsx,md,mdx}',
+              '**/_*/**',
+              '**/*.test.{js,jsx,ts,tsx}',
+              '**/__tests__/**',
+            ],
+            postsPerPage: 'ALL',
+            blogListComponent: '@theme/BlogListPage',
+            blogPostComponent: '@theme/BlogPostPage',
+            blogTagsListComponent: '@theme/BlogTagsListPage',
+            blogTagsPostsComponent: '@theme/BlogTagsPostsPage',
+            remarkPlugins: [(await import('remark-math')).default],
+            rehypePlugins: [],
+            beforeDefaultRemarkPlugins: [],
+            beforeDefaultRehypePlugins: [],
+            truncateMarker: /<!--\s*(truncate)\s*-->/,
+            showReadingTime: true,
+          },
+        }
+      ],
+    ],
+    plugins: [
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      '@docusaurus/plugin-ideal-image',
+    ],
+  };
+};
